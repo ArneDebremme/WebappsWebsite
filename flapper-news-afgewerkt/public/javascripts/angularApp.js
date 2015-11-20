1172f19279bdad1e7ef5ee0a6fdefad0c0f93937
yoloswagings <BREMME@MacBook-Pro-van-De.local>
2015-11-20 20:14:10 +0100

    slider

--- flapper-news-afgewerkt/public/javascripts/angularApp.js
+++ flapper-news-afgewerkt/public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', ['ui.router','ngMaterial']);//externe modules als dependency zetten
+var app = angular.module('flapperNews','sliderApp' ,['ui.router','ngMaterial','ngAnimate']);//externe modules als dependency zetten
 
 app.config(['$stateProvider', '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
@@ -50,6 +50,55 @@ function($stateProvider, $urlRouterProvider) {
 	})
 	$urlRouterProvider.otherwise('home');
 }]);
+app.directive('slider', function ($timeout) {
+	return {
+		restrict: 'AE',
+		replace: true,
+		scope:{
+			images: '='
+		},
+		link: function (scope, elem, attrs) {
+
+			scope.currentIndex=0;
+
+			scope.next=function(){
+				scope.currentIndex<scope.images.length-1?scope.currentIndex++:scope.currentIndex=0;
+			};
+
+			scope.prev=function(){
+				scope.currentIndex>0?scope.currentIndex--:scope.currentIndex=scope.images.length-1;
+			};
+
+			scope.$watch('currentIndex',function(){
+				scope.images.forEach(function(image){
+					image.visible=false;
+				});
+				scope.images[scope.currentIndex].visible=true;
+			});
+
+			/* Start: For Automatic slideshow*/
+
+			var timer;
+
+			var sliderFunc=function(){
+				timer=$timeout(function(){
+					scope.next();
+					timer=$timeout(sliderFunc,5000);
+				},5000);
+			};
+
+			sliderFunc();
+
+			scope.$on('$destroy',function(){
+				$timeout.cancel(timer);
+			});
+
+			/* End : For Automatic slideshow*/
+
+		},
+		templateUrl:'templates/templateurl.html'
+	}
+});
 
 app.factory('auth', ['$http', '$window',
 function($http, $window) {
@@ -194,7 +243,7 @@ function($scope, posts, auth) {
 	$scope.posts = posts.posts;
 	$scope.isLoggedIn = auth.isLoggedIn;
 	$scope.lal = auth.user;
-
+	$scope.images=[{src:'img1.png',title:'Pic 1'},{src:'img2.jpg',title:'Pic 2'},{src:'img3.jpg',title:'Pic 3'},{src:'img4.png',title:'Pic 4'},{src:'img5.png',title:'Pic 5'}];
 	//setting title to blank here to prevent empty posts
 	$scope.title = '';
 	$scope.description = '';
